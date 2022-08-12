3a0b392e9a242920e2a846fc8a1b483f7e25cfdb
Alberto Caminos <albeerc@gmail.com>
2022-08-12 20:50:04 -0300

    Sliders con librerías

--- js/app.js
+++ js/app.js
@@ -16,33 +16,32 @@ class Producto {
 
 const productos = [];
 
-productos.push (new Producto( "Air Jordan", "1 Low", 42000));
-productos.push (new Producto( "Air Jordan", "1 University", 75000));
-productos.push (new Producto( "Air Jordan", "6 Retro", 70000));
-productos.push (new Producto( "Air Jordan", "4 Retro", 55000));
-productos.push (new Producto( "Air Jordan", "11 Retro", 75000));
-productos.push (new Producto( "Air Jordan", "1 Clasic", 47000));
+productos.push (new Producto("Air Jordan", "1 University", 75000));
+productos.push (new Producto("Air Jordan", "1 Low", 42000));
+productos.push (new Producto("Air Jordan", "6 Retro", 70000));
+productos.push (new Producto("Air Jordan", "4 Retro", 55000));
+productos.push (new Producto("Air Jordan", "11 Retro", 75000));
+productos.push (new Producto("Air Jordan", "1 Clasic", 47000));
 
-productos.push (new Producto( "Nike", "Air Max 90", 25000));
-productos.push (new Producto( "Nike", "Air 270", 22000));
-productos.push (new Producto( "Nike", "Air Force 1 low", 17000));
-productos.push (new Producto( "Nike", "Air Max 97", 35000));
-productos.push (new Producto( "Nike", "Vapormax", 48000));
-productos.push (new Producto( "Nike", "Dunk High", 38000));
+productos.push (new Producto("Nike", "Air Max 90", 25000));
+productos.push (new Producto("Nike", "Air 270", 22000));
+productos.push (new Producto("Nike", "Air Force 1 low", 17000));
+productos.push (new Producto("Nike", "Air Max 97", 35000));
+productos.push (new Producto("Nike", "Vapormax", 48000));
+productos.push (new Producto("Nike", "Dunk High", 38000));
 
-productos.push (new Producto( "Adidas", "EQ 21", 26000));
-productos.push (new Producto( "Adidas", "Yeezy 350", 85000));
-productos.push (new Producto( "Adidas", "Yeezy 700", 70000));
-productos.push (new Producto( "Adidas", "Forum", 31000));
-productos.push (new Producto( "Adidas", "Yeezy 500", 43000));
-productos.push (new Producto( "Adidas", "Superstar", 25000));
+productos.push (new Producto("Adidas", "EQ 21", 26000));
+productos.push (new Producto("Adidas", "Yeezy 350", 85000));
+productos.push (new Producto("Adidas", "Yeezy 700", 70000));
+productos.push (new Producto("Adidas", "Forum", 31000));
+productos.push (new Producto("Adidas", "Yeezy 500", 43000));
+productos.push (new Producto("Adidas", "Superstar", 25000));
 
-productos.push (new Producto( "Puma", "Softride Enzo NXT", 17000));
-productos.push (new Producto( "Puma", "R78", 16000));
-productos.push (new Producto( "Puma", "Flyer Flex", 13000));
-productos.push (new Producto( "Puma", "Smash V2", 12000));
+productos.push (new Producto("Puma", "Softride Enzo NXT", 17000));
+productos.push (new Producto("Puma", "R78", 16000));
+productos.push (new Producto("Puma", "Flyer Flex", 13000));
+productos.push (new Producto("Puma", "Smash V2", 12000));
 
-console.log(...productos);
 
 
 // Creo la función general para guardar productos en el localstorage
@@ -75,3 +74,36 @@ for (const producto of productos) {
 for (const producto of productos) {
     producto.precioDescuento();
 }
+
+
+
+// Slider con Vegas JS
+$(".inicio").vegas({
+    slides: [
+        { src: "../img/slider1.png" },
+        { src: "../img/slider2.png" },
+        { src: "../img/slider3.png" }
+    ],
+    transition: 'blur'
+});
+
+
+//Slider con Slick JS
+$(".productos-destacados").slick({
+    infinity: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    draggable: false,
+    pauseOnHover: false,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+  });
