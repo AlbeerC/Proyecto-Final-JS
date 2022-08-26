cda4c081de4fb5a568e45b7b4b19d360e72c6825
Alberto Caminos <albeerc@gmail.com>
2022-08-26 14:09:55 -0300

    Carrito con JS

--- js/app.js
+++ js/app.js
@@ -1,98 +1,131 @@
-class Producto {
-    constructor( marca, modelo, precio) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.precio = precio;
+// Variables principales
+
+const lista = document.querySelector(".lista-productos");
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const btnVaciar = document.getElementById("vaciarCarrito");
+const contadorCarrito = document.getElementById("contador");
+const cantidad = document.getElementById("cantidad");
+const precioTotal = document.getElementById("precioTotal");
+
+let carrito = [];
+
+// Guardar productos del carrito desde el LocalStorage
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("carrito")) {
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+        actualizarCarrito();
+    }
+})
+
+// Generar el HTML desde un archivo JSON local
+
+fetch("../data/stock.json")
+    .then( (res) => res.json())
+    .then( (data) => {
+    data.forEach((producto) => {
+        const item = document.createElement("div")
+        item.className = "item"
+        item.innerHTML = `
+            <h3>${producto.marca} ${producto.modelo}</h3>
+            <img src="${producto.imagen}"></img>
+            <p>$${producto.precio}</p>
+            <button id="agregar${producto.id}" class="btnAgregar">Añadir al carrito <i class="fa-solid fa-cart-shopping"></i></i></button>
+        `
+
+        lista.appendChild(item);
+
+        // Evento de los botones para agregar al carrito
+        const btnAgregar = document.getElementById(`agregar${producto.id}`);
+        btnAgregar.addEventListener("click", () => {
+            añadirAlCarrito(producto.id)
+        })
+
+    })
+
+    // Funcion para agregar productos al carrito. Si ya existe, se suma a la cantidad, si no existe lo agrego
+    function añadirAlCarrito (prodId) {
+
+        const existe = carrito.some (prod => prod.id === prodId)
+            if (existe) {
+                carrito.map (prod => {
+                    if (prod.id === prodId) {
+                        prod.cantidad++
+                    }
+                })
+            } else {
+                const item = data.find((prod) => prod.id === prodId);
+                carrito.push(item);
+            }
+        
+        actualizarCarrito();
     }
 
-    precioIva() {
-        this.precio = this.precio * 1.21;
-    }
+    }) // Cierra el fetch
 
-    precioDescuento() {
-        this.precio = this.precio  - (this.precio * (5 / 100));
-    }
+
+
+// Función para actualizar el carrito cuando se agregue un producto
+function actualizarCarrito() {
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((prod) => {
+        const item = document.createElement("div")
+        item.className = ("productoCarrito")
+        item.innerHTML = `
+        <p>${prod.marca} ${prod.modelo}</p>
+        <p>Precio: $${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick = "eliminarCarrito(${prod.id})" class="btnEliminar"><i class="fa-solid fa-trash-can"></i></button>
+        `
+    contenedorCarrito.appendChild(item)
+
+    // Recuperar productos del carrito desde el LocalStorage
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+
+    })
+
+    // Calcular el precio total teniendo en cuenta las cantidades
+    contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
 }
 
-const productos = [];
-
-productos.push (new Producto("Air Jordan", "1 University", 75000));
-productos.push (new Producto("Air Jordan", "1 Low", 42000));
-productos.push (new Producto("Air Jordan", "6 Retro", 70000));
-productos.push (new Producto("Air Jordan", "4 Retro", 55000));
-productos.push (new Producto("Air Jordan", "11 Retro", 75000));
-productos.push (new Producto("Air Jordan", "1 Clasic", 47000));
-
-productos.push (new Producto("Nike", "Air Max 90", 25000));
-productos.push (new Producto("Nike", "Air 270", 22000));
-productos.push (new Producto("Nike", "Air Force 1 low", 17000));
-productos.push (new Producto("Nike", "Air Max 97", 35000));
-productos.push (new Producto("Nike", "Vapormax", 48000));
-productos.push (new Producto("Nike", "Dunk High", 38000));
-
-productos.push (new Producto("Adidas", "EQ 21", 26000));
-productos.push (new Producto("Adidas", "Yeezy 350", 85000));
-productos.push (new Producto("Adidas", "Yeezy 700", 70000));
-productos.push (new Producto("Adidas", "Forum", 31000));
-productos.push (new Producto("Adidas", "Yeezy 500", 43000));
-productos.push (new Producto("Adidas", "Superstar", 25000));
-
-productos.push (new Producto("Puma", "Softride Enzo NXT", 17000));
-productos.push (new Producto("Puma", "R78", 16000));
-productos.push (new Producto("Puma", "Flyer Flex", 13000));
-productos.push (new Producto("Puma", "Smash V2", 12000));
-
-
-
-// Creo la función general para guardar productos en el localstorage
-const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
-
-// Recorre todos los productos y los va guardando
-for (const producto of productos) {
-    guardarLocal(producto.modelo, JSON.stringify(producto));
+// Función para eliminar un producto del carrito
+function eliminarCarrito(prodId) {
+    const item = carrito.find((prod) => prod.id === prodId);
+    const i = carrito.indexOf(item);
+    carrito.splice(i, 1);
+    actualizarCarrito();
 }
 
-// Guardar el array entero
-guardarLocal("listaEntera", JSON.stringify(productos));
-
-
-//Obtener la lista de productos guardados
-const guardados = JSON.parse(localStorage.getItem("listaEntera"));
-const carrito = [];
-
-//Transformar todos los objetos a tipo producto
-for (const objeto of guardados) {
-    carrito.push(new Producto(objeto));
-}
-
-// Usar los métodos
-
-for (const producto of productos) {
-    producto.precioIva();
-}
-
-for (const producto of productos) {
-    producto.precioDescuento();
-}
+// Evento para que funcione el botón de vaciar el carrito
+btnVaciar.addEventListener("click", () => {
+    carrito.length = 0;
+    actualizarCarrito();
+})
 
 
 
-//Slider con Slick JS
-$(".productos-destacados").slick({
-    infinity: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false,
-    draggable: false,
-    pauseOnHover: false,
-    responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
-  });
+// Modal
+
+const contenedorModal = document.getElementsByClassName("contenedorModal")[0]
+const btnAbrir = document.getElementById("btnCarrito")
+const btnCerrar = document.getElementById("btnCerrar")
+const carritoModal = document.getElementsByClassName("carritoModal")[0]
+
+
+btnAbrir.addEventListener('click', () => {
+    contenedorModal.classList.toggle('modal-active')
+});
+
+btnCerrar.addEventListener('click', () => {
+    contenedorModal.classList.toggle('modal-active')
+});
+
+contenedorModal.addEventListener('click', () => {
+    contenedorModal.classList.toggle('modal-active')
+});
+
+carritoModal.addEventListener('click', (e) => {
+    e.stopPropagation()
+});
---
+++ js/librerias.js
@@ -0,0 +1,60 @@
+//Slider con Slick JS
+
+$(".productos-destacados").slick({
+  infinity: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  draggable: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+// Alerta con Sweet Alert 2
+
+function btnAlerta() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: '¿Estas seguro?',
+    text: "¿Quieres finalizar la compra?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Si, finalizar!',
+    cancelButtonText: 'No, cancelar!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Compra finalizada!',
+        'Muchas gracias por su compra!!',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado!',
+        'La compra ha sido cancelada',
+        'error'
+      )
+    }
+  })
+}
